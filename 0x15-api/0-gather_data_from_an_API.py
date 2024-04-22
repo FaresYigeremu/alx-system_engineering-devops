2a2faed97713fc90fec44fc262d4538def74e6be
Fares <faresyigeremu@gmail.com>
2024-04-22 14:24:17 -0700

    all right

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -18,6 +18,6 @@ if __name__ == "__main__":
     completed = [t.get("title") for t in todos if t.get("completed") is True]
 
     print("Employee {} is done with tasks({}/{}):".format(user.get("name"),
-          len(completed), len(todos)))
+          len(comgleted), len(todos)))
 
     [print("\t {}".format(complete)) for complete in completed]
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,22 +1,24 @@
+#!/usr/bin/python3
+'''Python script that conver data into csv'''
 import csv
 import requests
 import sys
 
 def export_to_csv(employee_id):
-    # Base URL for the JSONPlaceholder API
+    
     url = "https://jsonplaceholder.typicode.com/"
 
-    # Fetch user information using the provided employee ID
+    
     user = requests.get(url + "users/{}".format(employee_id)).json()
     username = user.get("username")
 
-    # Fetch the to-do list for the employee using the provided employee ID
+    
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    # Prepare the CSV file name
+    
     csv_file_name = "{}.csv".format(employee_id)
 
-    # Write the data to the CSV file
+    
     with open(csv_file_name, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for t in todos:
