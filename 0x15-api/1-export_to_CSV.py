c9626d730bd1c2503bf1a199a45a0b3b407a02ce
Fares <faresyigeremu@gmail.com>
2024-04-22 14:25:10 -0700

    pycode validation

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,25 +4,24 @@ import csv
 import requests
 import sys
 
+
 def export_to_csv(employee_id):
-    
+
     url = "https://jsonplaceholder.typicode.com/"
 
-    
     user = requests.get(url + "users/{}".format(employee_id)).json()
     username = user.get("username")
 
-    
     todos = requests.get(url + "todos", params={"userId": employee_id}).json()
 
-    
     csv_file_name = "{}.csv".format(employee_id)
 
-    
     with open(csv_file_name, "w", newline="") as csvfile:
         writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for t in todos:
-            writer.writerow([employee_id, username, t.get("completed"), t.get("title")])
+            writer.writerow(
+                [employee_id, username, t.get("completed"), t.get("title")])
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
