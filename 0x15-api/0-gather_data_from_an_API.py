e6a807671f3a14865e3e9c59bce92d84d744a18c
Fares <faresyigeremu@gmail.com>
2024-04-24 14:20:05 -0700

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,21 +1,43 @@
 #!/usr/bin/python3
-"""fetch  to-do list info from employee ID."""
+"""Fetch to-do list info from employee ID."""
 import requests
 import sys
+
+
+def fetch_employee_info(user_id):
+    """Fetch employee information from the API."""
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    response = requests.get(url)
+    return response.json()
+
+
+def fetch_todos(user_id):
+    """Fetch to-do list for the given employee ID."""
+    url = "https://jsonplaceholder.typicode.com/todos"
+    response = requests.get(url, params={"userId": user_id})
+    return response.json()
+
+
+def get_completed_tasks(todos):
+    """Filter and return completed tasks."""
+    return [todo.get("title") for todo in todos if todo.get("completed")]
+
+
+def print_todo_info(user, todos):
+    """Print employee's name and completed tasks."""
+    completed_tasks = get_completed_tasks(todos)
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed_tasks), len(todos)))
+    for task in completed_tasks:
+        print("\t{}".format(task))
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("Usage: python script.py <employee_id>")
         sys.exit(1)
 
-    url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
-
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    for c in completed:
-        print("\t {}".format(c))
-
+    user = fetch_employee_info(user_id)
+    todos = fetch_todos(user_id)
+    print_todo_info(user, todos)
