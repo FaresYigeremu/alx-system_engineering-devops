3e40decf1771dc76d531d09dca249dc5a07e85c3
Fares <119789181+FaresYigeremu@users.noreply.github.com>
2024-08-14 00:31:03 +0300

    fixed the prob

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,22 +1,25 @@
 #!/usr/bin/python3
-"""a function that queries the Reddit API and returns the number of subscribers"""
+"""function queries the Reddit API and returns the number of subscribers"""
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
+
     headers = {"User-Agent": "your_custom_user_agent"}
-    
+
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
+        response = requests.get(url, headers=headers)
+
         if response.status_code == 200:
             data = response.json()
-            
+
             subscriber_count = data['data']['subscribers']
-            
+
             return int(subscriber_count)
         else:
             return 0
     except Exception as e:
+
         return 0
+
